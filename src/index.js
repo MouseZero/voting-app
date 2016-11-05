ad30ba02744c1758cbb29cd94ce749d6a7f2256f
mousezero <rm@mousezero.com>
2016-11-04 19:30:02 -0700

    Added store config as well as reducer file

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import {render} from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/voteStyle.scss';
 
+
 render(
-  <Provider>
+  <Provider store="">
     <Router routes={routes} />
   </Provider>, document.getElementById('app')
 );
