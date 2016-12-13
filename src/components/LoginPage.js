4bd0524f0e0a31b39ed7859649d709e0415784b6
mousezero <rm@mousezero.com>
2016-12-12 22:52:44 -0800

    adding a form into the login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -13,6 +13,11 @@ module.exports = function () {
   return (
     <div>
       <h1>Login</h1>
+      <form>
+        User: <input ref='iUser' type='text'></input>
+        <br />
+        Password: <input ref='iPass' type='text'></input>
+      </form>
       You currently are {!auth.loggedIn() && "not "}logged in.
       <button onClick={()=>login()}>
         Log On
