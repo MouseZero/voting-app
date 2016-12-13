e69a91c4271af1ecdd53342ebf7b5b6033ea4c33
mousezero <rm@mousezero.com>
2016-12-12 22:56:22 -0800

    converted LoginPage to a class

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import auth from '../helpers/auth.js'
 
-module.exports = function () {
-  function login(){
+class LoginPage extends React.Component {
+  login(){
     auth.login('testuser', 'password');
   }
 
-  function logout(){
+  logout(){
     auth.logout();
   }
 
-  return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        User: <input ref='iUser' type='text'></input>
-        <br />
-        Password: <input ref='iPass' type='text'></input>
-      </form>
-      You currently are {!auth.loggedIn() && "not "}logged in.
-      <button onClick={()=>login()}>
-        Log On
-      </button>
-      <button onClick={()=>logout()}>
-        Log Out
-      </button>
-    </div>
-  )
+  render(){
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          User: <input ref='iUser' type='text'></input>
+          <br />
+          Password: <input ref='iPass' type='text'></input>
+        </form>
+        You currently are {!auth.loggedIn() && "not "}logged in.
+        <button onClick={()=>login()}>
+          Log On
+        </button>
+        <button onClick={()=>logout()}>
+          Log Out
+        </button>
+      </div>
+    )
+  }
+
 }
+
+module.exports = LoginPage
