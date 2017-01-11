fc41e18449a0436956acc0c60b9b926f1519b7b6
mousezero <rm@mousezero.com>
2017-01-10 19:28:08 -0800

    LoginPage access to redux store

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import auth from '../helpers/auth.js';
 import loginActions from '../actions/loginActions';
+import { objectToString } from '../debugging/util'
 
 class LoginPage extends React.Component{
   render(){
+    console.log(this.props.token);
     return (
       <div>
         User: <input type="text" ref={node => {this.nameInput = node}}></input>
         <br />
         Password: <input type="password" ref={node => this.passwordInput = node}></input>
         <br />
-        <button onClick={()=>alert(this.nameInput.value)} >LogIn</button>
+        <button onClick={()=>alert(this.nameInput.value + ' and ' + this.passwordInput.value)} >LogIn</button>
+        <br />
+        {Object.keys(this.props).map(elem => elem + ", ")}
       </div>
-    )
+      )
   }
 }
 
-export default connect()(LoginPage);
+const mapStateToProps = (state) => {
+  return {
+    token: state.login.token
+  }
+}
+
+module.exports = connect(mapStateToProps)(LoginPage);
