bd4d19cb089b8ac13d9399aea5e66cb6525a4f8f
mousezero <rm@mousezero.com>
2017-01-10 19:27:33 -0800

    debug code to print objects

--- src/debugging/util.js
+++ src/debugging/util.js
@@ -1,12 +1,16 @@
-function printObject(obj, depth, spacesToIndent = 0){
-  if(depth < 1 || typeof obj === 'string') return "";
+function objectToString(obj, depth, spacesToIndent = 0){
+  if(depth < 1 || typeof obj === 'string' || obj == null) return "";
   const list = Object.keys(obj).map(key => {
     return " ".repeat(spacesToIndent)
       + key
       + ": "
       + obj[key]
       + '\n'
-      + printObject(obj[key], depth - 1, spacesToIndent + 2);
+      + objectToString(obj[key], depth - 1, spacesToIndent + 2);
   })
   return list.join("\n");
+}
+
+module.exports = {
+  objectToString: objectToString
 }
