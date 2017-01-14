6d6a05e880657a2dbc6fb8f6888dc0a6563aeddb
mousezero <rm@mousezero.com>
2017-01-13 21:26:22 -0800

    Added input boxes

--- src/components/CreatePollsPage.js
+++ src/components/CreatePollsPage.js
@@ -2,19 +2,31 @@ const React = require('react');
 import { connect } from 'react-redux';
 import auth from '../helpers/auth.js';
 
+const inputBox = function(props) {
+  return (
+    <div>
+      {props.msg}:&nbsp;
+      <input type="text"></input>
+    </div>
+  )
+}
+
 const createPollsPage = function(props) {
 
   return (
     <div>
-      <div>
-        title: <input type="text"></input>
-      </div>
-      <div>
-        desc: <input type="text"></input>
-      </div>
-      <div>
-        json obj: <input type="text"></input>
-      </div>
+      {inputBox({msg: 'title'})}
+      {inputBox({msg: 'desc'})}
+      {inputBox({msg: 'Point 1'})}
+      {inputBox({msg: 'Point 2'})}
+      {inputBox({msg: 'Point 3'})}
+      {inputBox({msg: 'Point 4'})}
+      {inputBox({msg: 'Point 5'})}
+      {inputBox({msg: 'Point 6'})}
+      {inputBox({msg: 'Point 7'})}
+      {inputBox({msg: 'Point 8'})}
+      {inputBox({msg: 'Point 9'})}
+      {inputBox({msg: 'Point 10'})}
       <div>
         {props.token}
       </div>
