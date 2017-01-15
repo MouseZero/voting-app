f49fe9af631adfe57aad1e54d866ecbacc985a78
mousezero <rm@mousezero.com>
2017-01-14 20:28:28 -0800

    create poll service request is working

--- src/helpers/backendInterface.js
+++ src/helpers/backendInterface.js
@@ -1,10 +1,10 @@
-const url = 'http://localhost:3333/api/authenticate';
+const baseUrl = 'http://localhost:3333/api/';
 
 module.exports = {
 
   getNewToken: (name, password) => {
     return new Promise((resolve, reject)=>{
-      $.ajax(url, {
+      $.ajax((baseUrl + 'authenticate'), {
         type: 'POST',
         dataType: 'json',
         data: {
@@ -29,17 +29,17 @@ module.exports = {
     return new Promise((resolve, reject) => {
       console.log('token', token);
       console.log('chart', chart);
-      $.ajax(url, {
+      $.ajax((baseUrl + 'create/chart'), {
         type: 'POST',
         dataType: 'json',
         data: {
           title: chart.title,
           desc: chart.desc,
-          data: chart.points
+          data: JSON.stringify(chart.points)
         },
         beforeSend: function (jqXHR){
           jqXHR.setRequestHeader('x-access-token', token);
-          jqXHR.setRequestHeader('Access-Control-Allow-Origin', '*');
+          jqXHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         }
       })
     });
