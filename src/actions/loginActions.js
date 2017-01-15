af1f966a963fbff08e5fb6a66a0c35d1a1c36f26
mousezero <rm@mousezero.com>
2017-01-15 15:13:14 -0800

    added action for adding chart

--- src/actions/loginActions.js
+++ src/actions/loginActions.js
@@ -5,5 +5,13 @@
 module.exports = {
   changeToken(token){
     return {type: 'CHANGE_TOKEN', token};
+  },
+
+  addChart(chartObject){
+    const {id, title, desc, points } = chartObject;
+    return {
+      type: 'ADD_CHART',
+      chart: { id, title, desc, points }
+    }
   }
 }
