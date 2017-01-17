d39824f490ba2c25cf55345cf05b84014e700f44
mousezero <rm@mousezero.com>
2017-01-16 21:37:01 -0800

    fixed some warnings

--- src/helpers/commonDispatchers.js
+++ src/helpers/commonDispatchers.js
@@ -1,15 +1,14 @@
-import { setChartAction } from '../actions/chartActions.js'
-import { getCharts } from '../helpers/backendInterface.js'
+import { setChartAction } from '../actions/chartActions.js';
+import { getCharts } from '../helpers/backendInterface.js';
 
 module.exports = {
 
   updateCharts: (token, dispatch) => {
-      console.log('updateChart', token)
       getCharts(token)
       .then(data => {
         dispatch(setChartAction(data.charts));
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
     }
 
-}
+};
