14e419e4dc3a484c452e2f59fdd1f1f1211de50d
mousezero <rm@mousezero.com>
2017-01-17 15:13:47 -0800

    You can now log out of the voting app

--- src/actions/loginActions.js
+++ src/actions/loginActions.js
@@ -1,5 +1,9 @@
 module.exports = {
   changeToken(token){
     return {type: 'CHANGE_TOKEN', token};
+  },
+
+  logout(token){
+    return {type: 'CHANGE_TOKEN', token: ''}
   }
 }
--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../actions/loginActions';
 
-function ProfilePage(){
-  return(
-    <div>
-      Profile Page
-    </div>
-  )
+class ProfilePage extends React.Component {
+  constructor(props){
+    super(props);
+    this.logoff = this.logoff.bind(this);
+  }
+
+  logoff(){
+    this.props.logout()
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Profile Page</h1>
+        <button onClick={this.logoff}>Log Off</button>
+      </div>
+    )
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    token: state.login.token
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: () => dispatch(logout())
+  }
 }
 
-export default ProfilePage;
+export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
