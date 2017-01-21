f19bf2418e64854edfa2053b4a05799d98d24dd4
mousezero <rm@mousezero.com>
2017-01-20 18:30:45 -0800

    Fixed export

--- src/helpers/commonDispatchers.js
+++ src/helpers/commonDispatchers.js
@@ -2,12 +2,10 @@ import { setChartAction } from '../actions/chartActions';
 import { getCharts } from '../helpers/backendInterface';
 import { log, LOW } from '../helpers/log';
 
-function updateCharts (token, dispatch){
+export function updateCharts (token, dispatch){
   getCharts(token)
     .then(data => {
       dispatch(setChartAction(data.charts));
     })
     .catch(err => log(err, LOW));
 }
-
-module.exports = {updateCharts}
