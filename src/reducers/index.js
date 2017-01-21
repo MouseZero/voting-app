9338e699705dc6e0d7fef627f25169480e6b4dfc
mousezero <rm@mousezero.com>
2017-01-21 15:31:11 -0800

    adding chart reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import {routerReducer} from 'react-router-redux';
 import login from './logginReducer';
 import charts from './chartsReducer';
+import chart from './chartReducer';
 
 const rootReducer = combineReducers({
   routing: routerReducer,
   login,
-  charts
+  charts,
+  chart
 });
 
 export default rootReducer;
