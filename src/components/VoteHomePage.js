ff8cbdff9b8dec0610097bf0c3f3af6e9eba80e0
mousezero <rm@mousezero.com>
2017-01-31 14:54:13 -0800

    Added fake chart on the main page.

--- src/components/VoteHomePage.js
+++ src/components/VoteHomePage.js
@@ -1,20 +1,42 @@
-import React from 'react';
-import BarChart from './BarChart';
+import React, { Component } from 'react';
+import rd3, { BarChart } from 'react-d3-library';
 
-function voteHomePage() {
-  return (
-    <div>
-      <h1>Voting App</h1>
-      <p>&nbsp;</p>
-      <p>
-        Create polls for anyone to vote on. This app will keep track of the votes and
-        will show you the voting pattern of the respondents.
-      </p>
-      <p>
-        <BarChart/>
-      </p>
-    </div>
-  );
+const data = {};
+data.width = 500;
+data.height = 300;
+data.dataSet = [
+  {label: 'apples', value: 25},
+  {label: 'oranges', value: 30},
+  {label: 'surfboards', value: 150}
+];
+data.margins = {top: 20, right: 20, bottom: 70, left: 40};
+data.yAxisLabel = 'Test Chart';
+data.fill = [];
+data.barClass = 'bar';
+
+
+
+class VoteHomePage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {d3: ''};
+  }
+
+  componentDidMount(){
+    this.setState({d3: data});
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Voting App</h1>
+        <p>&nbsp;</p>
+          Create polls for anyone to vote on. This app will keep track of the votes and
+          will show you the voting pattern of the respondents.
+          <BarChart data={this.state.d3} />
+      </div>
+    );
+  }
 }
 
- export default voteHomePage;
+export default VoteHomePage;
