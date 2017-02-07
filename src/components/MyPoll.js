fe4c1c833406215cf7138adc017c76e6d5dd6d33
mousezero <rm@mousezero.com>
2017-02-06 19:07:12 -0800

    fix code warnings

--- src/components/MyPoll.js
+++ src/components/MyPoll.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class MyPoll extends Component {
   constructor(props){
     super(props);
-    this.mydata = props.data
+    this.mydata = props.data;
   }
 
   componentDidMount(){
-    let svg = d3.select("svg")
+    let svg = d3.select("svg");
     let g = svg.append("g")
     .attr("transform", `translation("10", "10")`)
-    .attr("class", "bar")
+    .attr("class", "bar");
 
     g.selectAll(".bar")
       .data(this.mydata)
@@ -18,13 +18,17 @@ class MyPoll extends Component {
       .attr("width", 10)
       .attr("height", (d) => d.value * 10)
       .attr("x", (d, i) => i * 15)
-      .attr("y", d => 70 - d.value * 10)
+      .attr("y", d => 70 - d.value * 10);
   }
 
   render(){
     return(
       <svg/>
-    )
+    );
   }
 }
+MyPoll.propTypes = {
+  data: PropTypes.object
+};
+
 export default MyPoll;
