5c499d45de48582b101c511912641ca80e950d4c
mousezero <rm@mousezero.com>
2017-02-07 09:12:09 -0800

    Chart changes size based off of screen size on the fly

--- src/components/MyPoll.js
+++ src/components/MyPoll.js
@@ -5,20 +5,25 @@ class MyPoll extends Component {
     super(props);
     this.mydata = props.data;
     this.min = props.min;
+    this.drawChart = this.drawChart.bind(this);
+    this.refreshOnWindowSizeChange = this.refreshOnWindowSizeChange.bind(this);
   }
 
-  componentDidMount(){
+  drawChart(mydata){
+    console.log('call drawChart');
+    const baseElement = document.getElementById('chartContainer')
+    baseElement.querySelector("svg").innerHTML = '';
     const chartWidth = document.getElementById('chartContainer').clientWidth;
     const chartHeight = window.innerHeight / 2;
 
-    const dataInfo = this.mydata.reduce((p, x) => {
+    const dataInfo = mydata.reduce((p, x) => {
       const max = (p.max > x.value) ? p.max : x.value;
       const min = (p.min < x.value) ? p.min : x.value;
       return {max, min};
     }, {max: Number.MIN_VALUE, min: Number.MAX_VALUE});
 
     const widthScaler = d3.scaleLinear()
-    .domain([0, this.mydata.length])
+    .domain([0, mydata.length])
     .range([0, chartWidth]);
 
     const heightScaler = d3.scaleLinear()
@@ -32,12 +37,24 @@ class MyPoll extends Component {
     .attr("class", "bar");
 
     g.selectAll(".bar")
-      .data(this.mydata)
+      .data(mydata)
       .enter().append("rect")
       .attr("width", (d, i) => widthScaler(1)*0.75 )
       .attr("height", ({value}) => heightScaler(value))
       .attr("x", (d, i) => widthScaler(i))
       .attr("y", ({value}) => heightScaler(dataInfo.max) - heightScaler(value));
+  }
+
+  refreshOnWindowSizeChange(){
+    const mydata = this.mydata;
+    const drawChart = this.drawChart;
+    window.addEventListener("resize", ()=>drawChart(mydata));
+    console.log('adding event listenr')
+  }
+
+  componentDidMount(){
+    this.drawChart(this.mydata);
+    this.refreshOnWindowSizeChange();
   }
 
   render(){
@@ -57,6 +74,7 @@ class MyPoll extends Component {
         <div id="chartContainer">
           <svg/>
         </div>
+        <button onClick={()=>this.drawChart(this.mydata)}>Refresh State</button>
       </div>
     );
   }
