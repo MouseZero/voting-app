58bcf13e1cc14489914a02b50afc80772992b62b
mousezero <rm@mousezero.com>
2017-02-16 21:17:46 -0800

    Removing unneeded code

--- src/components/VoteHomePage.js
+++ src/components/VoteHomePage.js
@@ -3,11 +3,6 @@ import React, { Component } from 'react';
 class VoteHomePage extends Component {
   constructor(props){
     super(props);
-    this.state = {d3: ''};
-  }
-
-  componentDidMount(){
-    this.setState({d3: data});
   }
 
   render(){
