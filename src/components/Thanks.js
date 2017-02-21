5a05dcb3adee8d65b8e8f898a03c6d330155a47e
mousezero <rm@mousezero.com>
2017-02-21 11:10:59 -0800

    (Feat) After voting you are taken back to the poll page

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -1,7 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { browserHistory } from 'react-router';
 
 export default class Thanks extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+    browserHistory.push('/poll/' + this.props.params.pollid);
+  }
+
   render(){
     return (
       <div>
@@ -10,11 +18,6 @@ export default class Thanks extends Component {
     );
   }
 }
-
-// export default function Thanks(){
-//   return (
-//     <div>
-//       Thanks for voting!
-//     </div>
-//   );
-// }
+Thanks.propTypes = {
+  params: PropTypes.object
+};
